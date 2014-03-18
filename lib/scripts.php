7be0cc7c2bd4f6cc516af81e1f659fc72d9beb69
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2014-03-18 10:25:56 -0500

    add theme files

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd444a106128989e80c91f9a019acac4d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '159c360cbe6ffdc0ec41d8573757caf2');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), '0fc6af96786d8f267c8686338a34cd38', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), 'cb5c2b45213264c4f665d516c9acfedc', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,110 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
-  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
-<?php endwhile; ?>
+<?php
+
+// define how pages will display
+$sections = array(
+	'sort_order' => 'ASC',
+	'sort_column' => 'menu_order', //post_title
+	'hierarchical' => 1,
+	'child_of' => 0,
+	'parent' => 0,
+	'depth' => 1,
+	'exclude_tree' => '',
+	'number' => '',
+	'offset' => 0,
+	'post_type' => 'page',
+	'post_status' => 'publish'
+);
+
+$pages = get_pages($sections);
+//start loop
+foreach ($pages as $page_data) {
+    $content = apply_filters('the_content', $page_data->post_content);
+    $title = $page_data->post_title;
+    $slug = $page_data->post_name;
+?>
+
+<section class='<?php echo "$slug" ?>'>
+	<div class="inner-content" id='<?php echo "$slug" ?>'>
+			<?php echo "$content" ?>
+	</div>
+</section>
+
+<?php 
+} 
+?>
+
+<!-- Portfolio Modal Content -->
+<div id="portfolio-detail">
+
+<?php
+
+// define how pages will display
+$portfolioItems = array(
+	'sort_order' => 'ASC',
+	'sort_column' => 'menu_order', //post_title
+	'hierarchical' => 1,
+	'child_of' => 416,
+	'exclude_tree' => '',
+	'number' => '',
+	'offset' => 0,
+	'post_type' => 'page',
+	'post_status' => 'publish'
+);
+
+$pages = get_pages($portfolioItems);
+//start loop
+foreach ($pages as $page_data) {
+    $content = apply_filters('the_content', $page_data->post_content);
+    $title = $page_data->post_title;
+    $slug = $page_data->post_name;
+?>
+
+<section class='<?php echo "$slug" ?>'>
+	<div class="inner-content tab-pane" id='<?php echo "$slug" ?>'>
+			<?php echo "$content" ?>
+	</div>
+</section>
+
+<?php 
+} 
+?>
+
+</div>
+
+<!-- Resume Modal Content -->
+<div id="about-detail">
+
+<?php
+
+// define how pages will display
+$aboutItems = array(
+	'sort_order' => 'ASC',
+	'sort_column' => 'menu_order', //post_title
+	'hierarchical' => 1,
+	'child_of' => 19,
+	'exclude_tree' => '',
+	'number' => '',
+	'offset' => 0,
+	'post_type' => 'page',
+	'post_status' => 'publish'
+);
+
+$pages = get_pages($aboutItems);
+//start loop
+foreach ($pages as $page_data) {
+    $content = apply_filters('the_content', $page_data->post_content);
+    $title = $page_data->post_title;
+    $slug = $page_data->post_name;
+?>
+
+<section class='<?php echo "$slug" ?>'>
+	<div class="inner-content" id='<?php echo "$slug" ?>-modal'>
+			<?php echo "$content" ?>
+	</div>
+</section>
+
+<?php 
+} 
+?>
+
+</div>
