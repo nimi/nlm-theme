0d35823dde5eaf80485e9b0d2f089c4f9095322a
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2014-03-23 22:32:42 -0500

    fix padding and font-size for headers

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f3503b76d0fa1b01251073254defed18');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '21e05e1d6a16067032d85acb798b5ba5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
