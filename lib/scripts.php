1895193456a169900646214ec75f332581497750
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2014-03-23 22:45:15 -0500

    decrease bottom padding in about section

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '21e05e1d6a16067032d85acb798b5ba5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'be387ebe0ea52c6f77b43c9bad4d8d97');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
